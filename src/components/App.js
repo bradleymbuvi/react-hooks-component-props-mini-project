e273cb47d60add7cf80582bd106515a311b8375a
bradleymbuvi <bradwent@gmail.com>
2024-02-07 23:35:31 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import blogData from "../data/blog";
 
+import About from "./About";
+import Header from "./Header";
+import ArticleList from "./ArticleList";
+
 console.log(blogData);
 
 function App() {
@@ -8,6 +12,9 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      {Header(blogData)}
+      {About(blogData)}
+      {ArticleList(blogData)}
     </div>
   );
 }
